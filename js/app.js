d9cfb70e46cd64bc6f084e3fc41f1e852959f506
prestonevans <89256614+prestonevans@users.noreply.github.com>
2022-08-04 13:50:54 -0600

    made dynamic stepper component

--- js/app.js
+++ js/app.js
@@ -1,2 +1,37 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const prevButton = document.querySelectorAll('button')[0];
+const nextButton = document.querySelectorAll('button')[1];
+const circleOutline = document.querySelectorAll('.outline');
+const completeLine = document.querySelector('.complete-line');
+let step = 0;
+
+nextButton.addEventListener('click', () => {
+  if (step === 3) return;
+  step++;
+  prevButton.disabled = false;
+  completeLine.style.width = `${step * 33}%`
+  circleOutline.forEach((circle, index) => {
+    if (index <= step) {
+      circle.classList.add('complete');
+    }
+    if (step === 3) {
+      nextButton.disabled = true;
+    }
+  });
+});
+
+prevButton.addEventListener('click', () => {
+  if (step === 0) return;
+  step--;
+  nextButton.disabled = false;
+  completeLine.style.width = `${step * 33}%`
+  circleOutline.forEach((circle, index) => {
+    if (index > step) {
+      circle.classList.remove('complete');
+    }
+    if (step !== 0) {
+      prevButton.disabled = false;
+    } else {
+      prevButton.disabled = true;
+    }
+  });
+});
